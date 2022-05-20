2dbd4f8cd63d8564ef1a5d417d6d6c34034572e8
Yurii Chepets <ychepets@gmail.com>
2022-05-20 14:43:28 +0300

    homework 08 first commit

--- src/SumAverage.java
+++ src/SumAverage.java
@@ -3,49 +3,72 @@
  */
 public class SumAverage {
 
-  /**
-   * Computes sum of running integers from a lower bound to an upper bound.
-   *
-   * @param lowerBound lower bound
-   * @param upperBound upper bound
-   * @return sum of integers
-   */
-  public static int sum(int lowerBound, int upperBound) {
-    // TODO fill in code here using for loop and replace the return statement
-    return -1;
-  }
+    /**
+     * Computes sum of running integers from a lower bound to an upper bound.
+     *
+     * @param lowerBound lower bound
+     * @param upperBound upper bound
+     * @return sum of integers
+     */
+    public static int sum(int lowerBound, int upperBound) {
+        int sum = 0;
+        if (lowerBound <= upperBound) {
+            for (int i = lowerBound; i <= upperBound; i++) {
+                sum += i;
+            }
+            return sum;
+        } else {
+            return 0;
+        }
+    }
 
-  /**
-   * Computes sum of running integers from a lower bound to an upper bound within an array.
-   *
-   * @param numbers running integers array
-   * @return sum of integers
-   */
-  public static int sum(int[] numbers) {
-    // TODO fill in code here using for each loop and replace the return statement
-    return -1;
-  }
+    /**
+     * Computes sum of running integers from a lower bound to an upper bound within an array.
+     *
+     * @param numbers running integers array
+     * @return sum of integers
+     */
+    public static int sum(int[] numbers) {
+        int sum = 0;
+        for (int i : numbers) {
+            sum += i;
+        }
+        return sum;
+    }
 
-  /**
-   * Computes average of running integers from a lower bound to an upper bound.
-   *
-   * @param lowerBound lower bound
-   * @param upperBound upper bound
-   * @return average of integers
-   */
-  public static double average(int lowerBound, int upperBound) {
-    // TODO fill in code here using while loop and replace the return statement
-    return -1;
-  }
+    /**
+     * Computes average of running integers from a lower bound to an upper bound.
+     *
+     * @param lowerBound lower bound
+     * @param upperBound upper bound
+     * @return average of integers
+     */
+    public static double average(int lowerBound, int upperBound) {
+        int sum = 0;
+        int count = 0;
+        while (lowerBound <= upperBound) {
+            sum += lowerBound;
+            lowerBound++;
+            count++;
+        }
+        return (double) sum / count;
+    }
 
-  /**
-   * Computes average of running integers from a lower bound to an upper bound within an array.
-   *
-   * @param numbers running integers array
-   * @return average of integers
-   */
-  public static double average(int[] numbers) {
-    // TODO fill in code here using do-while loop and replace the return statement
-    return -1;
-  }
+    /**
+     * Computes average of running integers from a lower bound to an upper bound within an array.
+     *
+     * @param numbers running integers array
+     * @return average of integers
+     */
+    public static double average(int[] numbers) {
+
+        // TODO fill in code here using do-while loop and replace the return statement
+        int sum = 0;
+        int i = 0;
+        do {
+            sum += numbers[i];
+            i++;
+        } while (i < numbers.length);
+        return (double) sum / numbers.length;
+    }
 }
--- src/SumAverageTest.java
+++ src/SumAverageTest.java
@@ -7,63 +7,65 @@ import org.testng.annotations.Test;
 
 public class SumAverageTest {
 
-  /**
-   * Test data for lower and upper bounds.
-   *
-   * @return test data array
-   */
-  @DataProvider(name = "Sum")
-  public static Object[][] sum() {
-    return new Object[][]{
-        {1, 100, 5050},
-        {-100, 100, 0},
-        {1, 1, 1}
-        // TODO add 2 more test data here
-    };
-  }
+    /**
+     * Test data for lower and upper bounds.
+     *
+     * @return test data array
+     */
+    @DataProvider(name = "Sum")
+    public static Object[][] sum() {
+        return new Object[][]{
+                {1, 100, 5050},
+                {-100, 100, 0},
+                {1, 1, 1},
+                {1, -5, 0},
+                {-3, 8, 30}
+        };
+    }
 
-  /**
-   * Test data for lower and upper bounds.
-   *
-   * @return test data array
-   */
-  @DataProvider(name = "Average")
-  public static Object[][] average() {
-    return new Object[][]{
-        {1, 100, 50.5},
-        {-100, 100, 0.0},
-        {1, 1, 1.0}
-        // TODO add 2 more test data here
-    };
-  }
+    /**
+     * Test data for lower and upper bounds.
+     *
+     * @return test data array
+     */
+    @DataProvider(name = "Average")
+    public static Object[][] average() {
+        return new Object[][]{
+                {1, 100, 50.5},
+                {-100, 100, 0.0},
+                {1, 1, 1.0},
+                {-3, 8, 2.5},
+                {0, 5, 2.5}
+        };
+    }
 
-  @Test(dataProvider = "Sum")
-  public void testSumArray(int lowerBound, int upperBound, int expectedResult) {
-    int actualResult = SumAverage.sum(lowerBound, upperBound);
+    @Test(dataProvider = "Sum")
+    public void testSumArray(int lowerBound, int upperBound, int expectedResult) {
+        int actualResult = SumAverage.sum(lowerBound, upperBound);
 
-    assertEquals(actualResult, expectedResult, "Sum is not correct");
-  }
+        assertEquals(actualResult, expectedResult, "Sum is not correct");
+    }
 
-  @Test(dataProvider = "Sum")
-  public void testSum1(int lowerBound, int upperBound, int expectedResult) {
-    int[] numbers = IntStream.range(lowerBound, upperBound + 1).toArray();
-    int actualResult = SumAverage.sum(numbers);
+    @Test(dataProvider = "Sum")
+    public void testSum1(int lowerBound, int upperBound, int expectedResult) {
+        int[] numbers = IntStream.range(lowerBound, upperBound + 1).toArray();
+        int actualResult = SumAverage.sum(numbers);
 
-    assertEquals(actualResult, expectedResult, "Sum is not correct");
-  }
+        assertEquals(actualResult, expectedResult, "Sum is not correct");
+    }
 
-  @Test(dataProvider = "Average")
-  public void testAverage(int lowerBound, int upperBound, double expectedResult) {
-    double actualResult = SumAverage.average(lowerBound, upperBound);
+    @Test(dataProvider = "Average")
+    public void testAverage(int lowerBound, int upperBound, double expectedResult) {
+        double actualResult = SumAverage.average(lowerBound, upperBound);
 
-    assertEquals(actualResult, expectedResult, "Average is not correct");
-  }
+        assertEquals(actualResult, expectedResult, "Average is not correct");
+    }
 
-  @Test(dataProvider = "Average")
-  public void testAverageArray(int lowerBound, int upperBound, double expectedResult) {
-    int[] numbers = IntStream.range(lowerBound, upperBound + 1).toArray();
-    double actualResult = SumAverage.average(numbers);
+    @Test(dataProvider = "Average")
+    public void testAverageArray(int lowerBound, int upperBound, double expectedResult) {
+        int[] numbers = IntStream.range(lowerBound, upperBound + 1).toArray();
+        double actualResult = SumAverage.average(numbers);
 
-    assertEquals(actualResult, expectedResult, "Average is not correct");
-  }
+        assertEquals(actualResult, expectedResult, "Average is not correct");
+    }
 }
